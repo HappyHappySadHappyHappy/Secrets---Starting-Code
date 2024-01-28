13d3ead8564e407fb63a2a3c8fe3ce393c7c08ae
HappyHappySadHappyHappy <harshitinani1@gmail.com>
2024-01-28 16:32:56 +0530

    testing

---
+++ app.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB"); 
+
+const schema  = new mongoose.Schema({
+    username : String,
+    password : String
+});
+
+schema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
+
+const user = new mongoose.model("user",schema);
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");  
+});
+
+app.post("/register",function(req,res){
+    const newuser = new user({
+        username:req.body.username,
+        password:req.body.password
+    });
+    newuser.save().then(function(){
+        res.render("secrets");
+    }).catch(function(err){
+        console.log("internal server error occured",err);
+    });
+});
+
+app.post("/login",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    user.findOne({'username':username}).then(function(found){
+        if(found){
+            if(found.password===password){
+                res.render("secrets");
+            }else{
+                res.send("wrong password")
+            }
+        }else{
+            res.send("user not found");
+        }
+    }).catch(function(err){
+        console.log("internal server error occured",err);
+    });
+});
+
+app.listen(3000,function(){
+    console.log("server is started at port 3000");
+});
